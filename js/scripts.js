69d04c556781a92f5efb5eee2a3a652ba05b485d
Dmitry Skorikov <dvskorikov@mail.ru>
2016-02-17 01:43:16 +0300

    исправление ошибок & доработка #1

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,7 @@ var feedback_link = document.querySelector(".feedback-btn");
 var overlay = document.querySelector(".overlay");
 var feedback = document.querySelector(".feedback-form");
 var feedback_form_close = document.querySelector(".feedback-form-close");
+var item_hover = document.querySelector(".quick-order-selected-item");
 
 
 feedback_link.addEventListener("click", function(event) {
@@ -14,4 +15,8 @@ feedback_form_close.addEventListener("click", function(event) {
   event.preventDefault();
   overlay.classList.remove("feedback-form-show");
   feedback.classList.remove("feedback-form-show");
+});
+
+item_hover.addEventListener("hover", function(event) {
+  item_hover.classList.add("quick-order-item-zind");
 });
