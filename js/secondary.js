ad4110be4b6da8ad863573a91e1b9718f5d12da4
Dmitry Skorikov <dvskorikov@mail.ru>
2016-02-18 03:22:12 +0300

    тестирование, доработка, js

--- js/secondary.js
+++ js/secondary.js
@@ -1,7 +1,3 @@
-var feedback_link = document.querySelector(".feedback-btn");
-var overlay = document.querySelector(".overlay");
-var feedback_form = document.querySelector(".feedback-form");
-var feedback_form_close = feedback_form.querySelector(".feedback-form-close");
 var login_form = document.querySelector(".top-menu-login-form");
 var login_btn = document.querySelector(".login-btn");
 var login = login_form.querySelector("[name=login-form-email]");
@@ -27,18 +23,6 @@ switch (slider) {
     }
 };
 
-feedback_link.addEventListener("click", function (event) {
-  event.preventDefault();
-  overlay.classList.add("feedback-form-show");
-  feedback_form.classList.add("feedback-form-show");
-});
-
-feedback_form_close.addEventListener("click", function (event) {
-  event.preventDefault();
-  overlay.classList.remove("feedback-form-show");
-  feedback_form.classList.remove("feedback-form-show");
-});
-
 login_btn.addEventListener("mouseout", function (event) {
   event.preventDefault();
   login_form.classList.remove("login-form-error");
@@ -62,29 +46,3 @@ login_form.addEventListener("submit", function (event) {
         login_form.classList.remove("login-form-error");
       }
 });
-
-window.addEventListener("keydown", function (event) {
-  if (event.keyCode === 27 && feedback_form.classList.contains("feedback-form-show")) {
-      overlay.classList.remove("feedback-form-show");
-      feedback_form.classList.remove("feedback-form-show");
-  }
-});
-
-overlay.addEventListener("click", function () {
-  overlay.classList.remove("feedback-form-show");
-  feedback_form.classList.remove("feedback-form-show");
-});
-
-
-slider1.addEventListener("click", function () {
-  localStorage.setItem("slider", "slider1");
-});
-
-slider2.addEventListener("click", function () {
-  localStorage.setItem("slider", "slider2");
-});
-
-slider3.addEventListener("click", function () {
-  localStorage.setItem("slider", "slider3");
-});
-
